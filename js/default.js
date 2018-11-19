b93498a93afcfe35ec031eaabf21a6d5d05a22bb
1727067 <38421425+1727067@users.noreply.github.com>
2018-11-19 18:08:02 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -38,3 +38,30 @@ for(key in player){
   li.textContent = key + ': ' + player[key];
   document.getElementById('object').appendChild(li);
 }
+
+var separate_time = function(time){
+  var sec   = Math.floor((time / 1000) % 60)Math.floor((time / 1000 / 60) % 60);
+  var min   = Math.floor((time / 1000 / 60) % 60);
+  var hours = Math.floor((time / 1000 / 60 / 60) % 24);
+  var days  = Math.floor( time / 1000 / 60 / 60 / 24);
+  return [sec, min, hours, days];
+}
+
+var update = function(){
+  var now = new Date();
+  var target = new Date(2020,7,24,0,0,0,0);
+  var diff = target.getTime() - now.getTime();
+  var counter = separate_time(now);
+  document.getElementById('countdown').textContent =
+    '東京オリンピックまであと ' +
+    counter[3] + '日' +
+    counter[2] + '時間' +
+    counter[1] + '分' +
+    counter[0] + '秒';
+  refresh();
+}
+
+var refresh= function(){
+  setTimeout(update, 1000);
+}
+update();
